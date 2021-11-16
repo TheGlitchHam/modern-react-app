19200de03a675464abea1e86917582e1064308ec
TheGlitchHam <faross@online.de>
2021-11-16 14:36:10 +0100

    basic file structure setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers'
+import { CTA, Brand, Navbar } from './components'
+
 const App = () => {
   return (
     <div>
