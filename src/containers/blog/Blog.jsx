18e7059ca892f6f1469ae7ca49b3cf8c7a4fb5f4
TheGlitchHam <faross@online.de>
2021-11-22 15:37:15 +0100

    implementing blog container and articles

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './blog.css'
+import {Article} from "../../components"
+import {blog01, blog02, blog03, blog04, blog05} from "./imports"
+
+
+const date = "Sep 26, 2021"
+const text = "GPT-3 and OpenAi is the future. Let us explore hot it is?"
 
 const Blog = () => {
     return (
-        <div>
-            
+        <div className="gpt3__blog section__padding" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">
+                    A lot is happening, <br/>We are blogging about it.    
+                </h1>    
+            </div>            
+
+                <div className="gpt3__blog-container">
+                    <div className="gpt3__blog-container_groupA">
+                    <Article imgUrl={blog01} date={date} text={text}  />
+                    </div>
+                    <div className="gpt3__blog-container_groupB">
+                        <Article imgUrl={blog02} date={date} text={text} />
+                        <Article imgUrl={blog03} date={date} text={text} />
+                        <Article imgUrl={blog04} date={date} text={text} />
+                        <Article imgUrl={blog05} date={date} text={text} />
+                    </div>
+                </div>
         </div>
     )
 }
